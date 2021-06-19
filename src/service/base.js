3429744d66a88f48984b50522883e84cf2350ddb
zhiyuIpos <zhiyu-@outlook.com>
2021-06-19 15:39:15 +0800

    version nginx 1.0.0

--- src/service/base.js
+++ src/service/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ERR_OK = 0
-const baseURL = process.env.NODE_ENV === 'production' ? 'http://101.34.48.243:80' : '/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://101.34.48.243/music/' : '/'
 
 axios.defaults.baseURL = baseURL
 
