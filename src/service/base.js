ad3d4a9e4fe7b27e09f08d21b82abb42bbf4662f
zhiyuIpos <zhiyu-@outlook.com>
2021-06-20 17:09:29 +0800

    version 9.0.0

--- src/service/base.js
+++ src/service/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ERR_OK = 0
-const baseURL = process.env.NODE_ENV === 'production' ? 'http://101.34.48.243/music/' : '/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://101.34.48.243/rinima/' : '/'
 
 axios.defaults.baseURL = baseURL
 
