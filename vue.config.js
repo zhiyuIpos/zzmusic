c4af04818d6c1c365e4e6d57f822edd5b06e66c8
zhiyuIpos <zhiyu-@outlook.com>
2021-06-20 18:33:51 +0800

    version 11.0.0

--- vue.config.js
+++ vue.config.js
@@ -27,5 +27,5 @@ module.exports = {
     }
   },
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/music/' : '/'
 }
